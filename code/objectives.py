0494b11ea757bbfaa5cfef37dbe51e9b63fa71f7
Amerikranian <olegjpittman@gmail.com>
2019-12-13 16:29:58 -0600

    Fixing the tuple error with objectives

--- code/objectives.py
+++ code/objectives.py
@@ -3,6 +3,7 @@ This file is only used for storing the player's objectives, nothing else"""
 import base_objective as b_o
 
 #Objective list
-objective_list = (,
+#Remove comment only if at least 1 objective exists in the objective_list
+#objective_list = (,
 
 )
